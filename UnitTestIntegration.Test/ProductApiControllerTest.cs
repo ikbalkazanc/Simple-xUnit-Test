adc8adf3374c596cd57ee51450fed6e3aeb137c1
ikbalkazanc <60016655+ikbalkazanc@users.noreply.github.com>
2021-01-19 05:09:28 +0300

    new tests

---
+++ UnitTestIntegration.Test/ProductApiControllerTest.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnitTestIntegration.Web.Controllers;
+using UnitTestIntegration.Web.Model;
+using UnitTestIntegration.Web.Repository;
+using Xunit;
+
+namespace UnitTestIntegration.Test
+{
+    public class ProductApiControllerTest
+    {
+        private readonly Mock<IRepository<Table>> _mockRepo;
+        private readonly ProductsApiController _controller;
+        //private readonly Helper _helper;
+        private List<Table> products;
+
+        public ProductApiControllerTest()
+        {
+            _mockRepo = new Mock<IRepository<Table>>();
+            _controller = new ProductsApiController(_mockRepo.Object);
+            //_helper = new Helper();
+            products = new List<Table>() { new Table { Id = 1, Name = "Kalem", Price = 100, Stock = 50 },
+            new Table { Id = 2, Name = "Defter", Price = 200, Stock = 500 }};
+        }
+        [Fact]
+        public async void GetProduct_ActionExecutes_ReturnOkResultWithProduct()
+        {
+            _mockRepo.Setup(x => x.GetAll()).ReturnsAsync(products);
+
+            var result = await _controller.GetProduct();
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+
+            var returnProducts = Assert.IsAssignableFrom<IEnumerable<Table>>(okResult.Value);
+
+            Assert.Equal<int>(2, returnProducts.ToList().Count);
+        }
+        [Theory]
+        [InlineData(0)]
+        public async void GetProduct_IdInValid_ReturnNotFound(int productId)
+        {
+            Table product = null;
+
+            _mockRepo.Setup(x => x.GetById(productId)).ReturnsAsync(product);
+
+            var result = await _controller.GetProduct(productId);
+
+            Assert.IsType<NotFoundResult>(result);
+        }
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public async void GetProduct_IdValid_ReturnOkResult(int productId)
+        {
+            var product = products.First(x => x.Id == productId);
+
+            _mockRepo.Setup(x => x.GetById(productId)).ReturnsAsync(product);
+
+            var result = await _controller.GetProduct(productId);
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+
+            var returnProduct = Assert.IsType<Table>(okResult.Value);
+
+            Assert.Equal(productId, returnProduct.Id);
+            Assert.Equal(product.Name, returnProduct.Name);
+        }
+        [Theory]
+        [InlineData(1)]
+        public void PutProduct_IdIsNotEqualProduct_ReturnBadRequestResult(int productId)
+        {
+            var product = products.First(x => x.Id == productId);
+
+            var result = _controller.PutProduct(2, product);
+
+            var badRequestResult = Assert.IsType<BadRequestResult>(result);
+        }
+        [Theory]
+        [InlineData(1)]
+        public void PutProduct_ActionExecutes_ReturnNoContent(int productId)
+        {
+            var product = products.First(x => x.Id == productId);
+
+            _mockRepo.Setup(x => x.Update(product));
+
+            var result = _controller.PutProduct(productId, product);
+
+            _mockRepo.Verify(x => x.Update(product), Times.Once);
+
+            Assert.IsType<NoContentResult>(result);
+        }
+        [Fact]
+        public async void PostProduct_ActionExecutes_ReturnCreatedAtAction()
+        {
+            var product = products.First();
+
+            _mockRepo.Setup(x => x.Create(product)).Returns(Task.CompletedTask);
+
+            var result = await _controller.PostProduct(product);
+
+            var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result);
+
+            _mockRepo.Verify(x => x.Create(product), Times.Once);
+
+            Assert.Equal("GetProduct", createdAtActionResult.ActionName);
+        }
+        [Theory]
+        [InlineData(0)]
+        public async void DeleteProduct_IdInValid_ReturnNotFound(int productId)
+        {
+            Table product = null;
+
+            _mockRepo.Setup(x => x.GetById(productId)).ReturnsAsync(product);
+
+            var resultNotFound = await _controller.DeleteProduct(productId);
+
+            Assert.IsType<NotFoundResult>(resultNotFound.Result);
+        }
+        [Theory]
+        [InlineData(1)]
+        public async void DeleteProduct_ActionExecute_ReturnNoContent(int productId)
+        {
+            var product = products.First(x => x.Id == productId);
+            _mockRepo.Setup(x => x.GetById(productId)).ReturnsAsync(product);
+            _mockRepo.Setup(x => x.Delete(product));
+
+            var noContentResult = await _controller.DeleteProduct(productId);
+
+            _mockRepo.Verify(x => x.Delete(product), Times.Once);
+
+            Assert.IsType<NoContentResult>(noContentResult.Result);
+        }
+
+    }
+}
---
+++ UnitTestIntegration.Web/Controllers/ProductsApiController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnitTestIntegration.Web.Model;
+using UnitTestIntegration.Web.Repository;
+
+namespace UnitTestIntegration.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsApiController : ControllerBase
+    {
+        private readonly IRepository<Table> _repository;
+
+        public ProductsApiController(IRepository<Table> repository)
+        {
+            _repository = repository;
+        }
+        /*
+        [HttpGet("{a}/{b}")]
+        public IActionResult Add(int a, int b)
+        {
+            return Ok(new Helpers.Helper().add(a, b));
+        }
+        */
+        // GET: api/ProductsApi
+        [HttpGet]
+        public async Task<IActionResult> GetProduct()
+        {
+            var products = await _repository.GetAll();
+
+            return Ok(products);
+        }
+
+        // GET: api/ProductsApi/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _repository.GetById(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        // PUT: api/ProductsApi/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public IActionResult PutProduct(int id, Table product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _repository.Update(product);
+
+            return NoContent();
+        }
+
+        // POST: api/ProductsApi
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<IActionResult> PostProduct(Table product)
+        {
+            await _repository.Create(product);
+
+            return CreatedAtAction("GetProduct", new { id = product.Id }, product);
+        }
+
+        // DELETE: api/ProductsApi/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Table>> DeleteProduct(int id)
+        {
+            var product = await _repository.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _repository.Delete(product);
+
+            return NoContent();
+        }
+
+        private bool ProductExists(int id)
+        {
+            Table product = _repository.GetById(id).Result;
+
+            if (product == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+
+}
